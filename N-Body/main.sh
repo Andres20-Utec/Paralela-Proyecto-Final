00ddc7573fa6285b9443d9b855e6dedb94336676
bhn16 <dario.toribio@utec.edu.pe>
2022-11-27 16:29:01 -0500

    d

--- N-Body/main.sh
+++ N-Body/main.sh
@@ -6,7 +6,7 @@ make cpu-4th
 for ((count = 4; count < 200; count*=2))
 do
   Data/./gen.out $count 1
-  for ((countt = 1; countt < 70; countt*=2))
+  for ((countt = 2; countt < 70; countt*=2))
   do
     mpirun -np $countt ./cpu-4th "Data/data-${count}.inp" > Valores/$(($count*1024))\_$countt.txt
   done
