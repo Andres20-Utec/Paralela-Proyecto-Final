9aac0c609dee825f872a36d7fc24b994ddd59677
bhn16 <dario.toribio@utec.edu.pe>
2022-11-27 19:39:03 -0500

    boton creado

--- Cliente/Cliente/src/App.jsx
+++ Cliente/Cliente/src/App.jsx
@@ -1,6 +1,7 @@
 import Topbar from './components/topbar/Topbar'
 import Navbar from './components/navbar/Navbar'
 import Graphic from './components/graphic/Graphic';
+import Boton from './components/boton/Boton';
 import { Route, Routes } from "react-router-dom";
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/fuerza" element={<Graphic imagen={"Tiempo-fuerza.png"} />} />
         <Route path="/total" element={<Graphic imagen={"Tiempo-total.png"} />} />
       </Routes >
+      <Boton />
     </>
   )
 }
---
+++ Cliente/Cliente/src/components/boton/Boton.jsx
@@ -0,0 +1,11 @@
+import "./Boton.css";
+
+export default function Boton() {
+  return (
+    <div className="box-boton">
+      <a href="/csv.zip">
+        <button className="button">Descargar csv's</button>
+      </a>
+    </div>
+  )
+}
